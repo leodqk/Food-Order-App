189b46a7ab54f4313bfc1723f94034b36de6d588
Dương Quang Khải <94659049+leodqk@users.noreply.github.com>
2024-07-28 09:59:01 +0700

    Complete_login_FE

---
+++ backend/src/models/user.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  auth0Id: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  AddressLine1: {
+    type: String,
+  },
+});
